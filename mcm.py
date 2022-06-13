57ab7e55b00540b5453ffd923420bdc9d9cfce9c
Ale-SaP <85176998+Ale-SaP@users.noreply.github.com>
2022-06-13 20:14:30 -0300

    Update mcm.py

--- mcm.py
+++ mcm.py
@@ -1,4 +1,4 @@
-def mcdRecursivo( numero ):
+def factoresRecursivo( numero ):
     if numero < 2:
         print(numero)
         return numero
@@ -6,21 +6,21 @@ def mcdRecursivo( numero ):
     elif (numero >= 2): 
         if (numero % 2 == 0):
             print(2)
-            return mcdRecursivo( numero//2 )
+            return factoresRecursivo( numero//2 )
 
         elif (numero % 3 == 0):
             print(3)
-            return mcdRecursivo( numero//3 )
+            return factoresRecursivo( numero//3 )
 
         else:
             for x in range(5, numero + 1, 2):
                 if (numero % x == 0):
                     print(x)
-                    return mcdRecursivo(numero // x)
+                    return factoresRecursivo(numero // x)
 
 
 
-def mcdIterativo( numero ):
+def factoresIterativo( numero ):
     while numero >= 2:
         if (numero % 2 == 0):
             numero = numero//2
@@ -36,46 +36,14 @@ def mcdIterativo( numero ):
 
 ListaDePrimos = []
 
-def mcdConMemoizacion( numero ):
+def factoresConMemoizacion( numero ):
     if numero < 2:
         print(numero)
         return numero
 
-    elif (numero >= 2): 
-        if (numero % 2 == 0):
-            print(2)
-            return mcdConMemoizacion( numero//2 )
-
-        elif (numero % 3 == 0):
-            print(3)
-            return mcdConMemoizacion( numero//3 )
-
-        else:
-            for x in ListaDePrimos:
-                if (numero % x == 0):
-                    print(x)
-                    return mcdConMemoizacion(numero // x)
-
-
-divisores = []
-def mcdLista( numero ):
-    global divisores
-    if numero < 2:
-        divisores.append(numero)
-        return divisores
-
-    elif (numero >= 2): 
-        if (numero % 2 == 0):
-            divisores.append(2)
-            return mcdLista( numero//2 )
-
-        elif (numero % 3 == 0):
-            divisores.append(3)
-            return mcdLista( numero//3 )
-
-        else:
-            for x in range(5, numero + 1, 2):
-                if (numero % x == 0):
-                    divisores.append(x)
-                    return mcdLista(numero // x)
+    else:
+        for x in ListaDePrimos:
+           if (numero % x == 0):
+              print(x)
+              return mcdConMemoizacion(numero // x)
              
