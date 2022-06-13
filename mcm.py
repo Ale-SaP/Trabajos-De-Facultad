395e3d16f5ecedbf127a0b8370cf2f75aff9a770
Ale-SaP <85176998+Ale-SaP@users.noreply.github.com>
2022-06-12 23:39:29 -0300

    Update mcm.py

--- mcm.py
+++ mcm.py
@@ -1,15 +1,15 @@
-def mcm( numero ):
+def mcd( numero ):
     if numero < 2:
         return numero
 
     elif (numero >= 2): 
         if (numero % 2 == 0):
-            return mcm( numero//2 )
+            return mcd( numero//2 )
 
         elif (numero % 3 == 0):
-            return mcm( numero//3 )
+            return mcd( numero//3 )
 
         else:
             for x in range(4, numero + 1 ):
                 if (numero % x == 0):
-                    return mcm(numero // x)
+                    return mcd(numero // x)
